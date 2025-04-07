83ca65ebf8c0703d01d7e38fdb712a56eaa0e13d
abdul-kodir2020 <mounirouabdul40@gmail.com>
2025-04-07 10:37:58 +0200

    Buffer => btoa

--- exo1/auth.js
+++ exo1/auth.js
@@ -1,12 +1,12 @@
-// ressource : https://stackoverflow.com/questions/38134200/base64-encode-a-javascript-object
-
 const generateToken = (user) => {
     const userStr = JSON.stringify(user)
-    return Buffer.from(userStr).toString("base64")
+    // return Buffer.from(userStr).toString("base64")
+    return btoa(userStr)
 }
 
 const verifyToken = (token) => {
-    const userStr = Buffer.from(token, "base64").toString()
+    // const userStr = Buffer.from(token, "base64").toString()
+    const userStr = atob(token)
     return JSON.parse(userStr)
 }
 
