f0ea167ba2214425d3ef58c20200a78357bc310e
abdul-kodir2020 <62393812+abdul-kodir2020@users.noreply.github.com>
2025-10-09 15:45:39 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoutes';
+//import ProtectedRoute from './components/ProtectedRoutes';
 import Register from './pages/Register';
 import { Toaster } from 'sonner'
 import Home from './pages/Home';
